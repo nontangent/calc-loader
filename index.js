35cfed1a62b2da0c41f636ac24475933170c755f
Nozomu Miyamoto <nontan@NozomunoMacBook-ea.local>
2020-06-01 13:10:53 +0900

    enable cache

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const loaderUtils = require('loader-utils');
 const functionsResolver = require('./functions-resolver');
 
 module.exports = function(source, map) {
-  // this.cacheable();
+  this.cacheable();
   const options = loaderUtils.getOptions(this);
 	const functions = options.functions || [];
 	source = functionsResolver(source, options.functions);
